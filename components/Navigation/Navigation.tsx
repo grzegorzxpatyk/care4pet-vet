b637e76ded57ff09cb1f801fb46b809ad1aa3563
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-18 23:47:36 +0200

    fix navigation links styling

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -6,7 +6,7 @@ export default function Navigation() {
       <ul className='flex flex-col justify-start gap-2'>
         {['patients', 'customers', 'inventory', 'settings'].map((element) => (
           <Link key={element} href={`/dashboard/${element}`}>
-            <li className='w-full rounded bg-blue-800/10 p-3 capitalize transition-colors hover:bg-blue-800/30 hover:text-blue-200'>
+            <li className='w-full rounded bg-blue-800/10 p-3 capitalize transition-colors hover:bg-blue-800/30 hover:text-blue-950 dark:hover:text-blue-200'>
               {element}
             </li>
           </Link>
