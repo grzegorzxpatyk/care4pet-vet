b057fefc6c9c5973a5f520ec0349446df8107c4b
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-28 01:30:23 +0200

    refactor: NavLinks route display when route includes '-' character

--- components/NavLinks/NavLinks.tsx
+++ components/NavLinks/NavLinks.tsx
@@ -9,20 +9,22 @@ export default function NavLinks() {
 
   return (
     <ul className='flex w-full flex-col justify-start gap-2'>
-      {['patients', 'customers', 'inventory', 'settings'].map((element) => (
-        <Link key={element} href={`/dashboard/${element}`}>
-          <li
-            className={clsx(
-              'w-full rounded bg-blue-800/10 p-3 capitalize transition-colors hover:bg-blue-800/30 hover:text-blue-950 dark:hover:text-blue-200',
-              {
-                'bg-blue-800/30': pathname === `/dashboard/${element}`,
-              }
-            )}
-          >
-            {element}
-          </li>
-        </Link>
-      ))}
+      {['patients', 'customers', 'health-record', 'inventory', 'settings'].map(
+        (route) => (
+          <Link key={route} href={`/dashboard/${route}`}>
+            <li
+              className={clsx(
+                'w-full rounded bg-blue-800/10 p-3 capitalize transition-colors hover:bg-blue-800/30 hover:text-blue-950 dark:hover:text-blue-200',
+                {
+                  'bg-blue-800/30': pathname === `/dashboard/${route}`,
+                }
+              )}
+            >
+              {route.includes('-') ? route.split('-').join(' ') : route}
+            </li>
+          </Link>
+        )
+      )}
     </ul>
   );
 }
