1512130132178273245d3dbdb54b8fc33e1bf33d
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-30 00:14:50 +0200

    create route handler for fetching resource name based on id in client components

---
+++ app/api/route.ts
@@ -0,0 +1,33 @@
+import { fetchResourceByIdAndTableName } from '../lib/data';
+import { isTableName, isUUID } from '../lib/utils';
+
+export const dynamic = 'force-dynamic'; // defaults to auto
+export async function GET(request: Request) {
+  if (
+    !request.headers.has('X-PATHNAME') &&
+    request.headers.get('X-PATHNAME') === null
+  ) {
+    throw new Error('There is no X-PATHNAME header provided.');
+  }
+  const pathname = request.headers.get('X-PATHNAME');
+  if (pathname === null) {
+    throw new Error('X-PATHNAME is null');
+  }
+  console.log(pathname);
+  const pathnameArray = pathname.split('/');
+  const id = pathnameArray.find((element) => isUUID(element));
+  if (!isUUID(id)) {
+    throw new Error(
+      'The provided id in X-PATHNAME header value is not in UUID format.'
+    );
+  }
+  const tableName = pathnameArray?.at(2);
+
+  if (!isTableName(tableName)) {
+    throw new Error('The category provided in pathname is unknown.');
+  }
+
+  const foundResource = await fetchResourceByIdAndTableName(id, tableName);
+
+  return Response.json({ id: foundResource.id, name: foundResource.name });
+}
