ea196d85d9630501a1417df712133c22654b5fb9
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-05-01 01:19:54 +0200

    add link to appLogo in dahsboard layout leading to home dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import AppLogo from '@/components/AppLogo/AppLogo';
 import Breadcrumbs from '@/components/Breadcrumbs/Breadcrumbs';
 import Navigation from '@/components/Navigation/Navigation';
 import type { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: {
@@ -18,7 +19,9 @@ export default function Layout({
   return (
     <div className='grid h-full w-full grid-cols-[280px_1fr] grid-rows-[100px_1fr]'>
       <div className='border-b border-r border-zinc-600 p-8'>
-        <AppLogo />
+        <Link href='/dashboard'>
+          <AppLogo />
+        </Link>
       </div>
       <div className='border-b border-zinc-600 p-8 text-2xl'>
         <Breadcrumbs />
