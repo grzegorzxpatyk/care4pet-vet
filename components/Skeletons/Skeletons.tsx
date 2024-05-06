d03bb3bd52dc6cf3f6b9a2efe77652cae5ab480c
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-05-07 00:39:23 +0200

    create skeleton for Breadcrumbs

--- components/Skeletons/Skeletons.tsx
+++ components/Skeletons/Skeletons.tsx
@@ -1,5 +1,6 @@
 import { Half2Icon } from '@radix-ui/react-icons';
 import Button from '../Button/Button';
+import { BreadcrumbItem, Breadcrumbs } from '@nextui-org/breadcrumbs';
 
 export function ThemeSwitcherSkeleton() {
   return (
@@ -17,3 +18,15 @@ export function ThemeSwitcherSkeleton() {
   );
 }
 
+export function BreadcrumbsSkeleton() {
+  return (
+    <Breadcrumbs
+      size='lg'
+      itemClasses={{ item: 'w-20 bg-zinc-500/30 rounded animate-pulse' }}
+    >
+      {['loading', 'loading', 'loading'].map((element, i) => (
+        <BreadcrumbItem key={i}>&nbsp;</BreadcrumbItem>
+      ))}
+    </Breadcrumbs>
+  );
+}
